a0855e8f8e1253d6cd6783a62301f6fc25737003
HaseebTJU <haseebabdurrahman@gmail.com>
2024-10-09 21:14:51 +0100

    Added encryption for passwords using bcrypt

--- server/routes/login.js
+++ server/routes/login.js
@@ -1,23 +1,37 @@
 const express = require('express'); 
 const router = express.Router();
 const pool = require('../config/db'); 
+const bcrypt = require('bcrypt'); 
 
 // logging in
 router.post("/", async (req, res) => {
     const { username, password } = req.body; 
 
-    try {
-        const response = await pool.query('SELECT * FROM WHERE username = $1', [username]); 
-        if (response.rows.length === 0) {
-            return res.status(400).json({ message: 'Incorrect username or password' });
-        }; 
-
-        res.status(200).json({ message: 'Login Successful!' })
-    } catch(e) {
-        console.error(e);
-        res.status(500).json({ message: "Server error"} );
+    if (!username || !password) {
+        return res.status(400).json({ message: 'Username and password are required' });
     }
 
+    try {
+        // Query for the user
+        const response = await pool.query('SELECT * FROM users WHERE username = $1', [username]); 
+        if (response.rows.length === 0) {
+            return res.status(400).json({ message: 'Incorrect username or password' });
+        } 
+
+        // Get the hashed password from the database
+        const hashedPassword = response.rows[0].password; 
+        const matchedPassword = await bcrypt.compare(password, hashedPassword);
+        
+        if (!matchedPassword) {
+            return res.status(400).json({ message: 'Incorrect username or password' }); 
+        } 
+        
+        res.status(200).json({ message: 'Login Successful!' });
+        
+    } catch(e) {
+        console.error(e);
+        res.status(500).json({ message: "Server error" });
+    }
 }); 
 
 module.exports = router;
--- server/routes/registration.js
+++ server/routes/registration.js
@@ -1,15 +1,27 @@
 const express = require("express"); 
 const router = express.Router(); 
 const pool = require('../config/db');
-
+const bcrypt = require('bcrypt'); 
 //registration
 
 router.post("/", async (req, res) => {
     const { body } = req;
     const { username, password } = body; 
+
+    if (!username || !password) {
+        return res.status(400).json({ message: 'Username and password are required' });
+    };
     
     try {
-        await pool.query('INSERT INTO users (username, password) VALUES ($1, $2)', [username, password]); 
+        const response = await pool.query('SELECT * FROM users WHERE username = $1', [username]); 
+        if (response.rows.length !== 0) {
+           return res.status(400).json('User already exists!');
+        } 
+
+        const salt = await bcrypt.genSalt(10); 
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        await pool.query('INSERT INTO users (username, password) VALUES ($1, $2)', [username, hashedPassword]); 
         res.status(201).json({ message: 'User registered successfully' }); 
     } catch (e) {
         console.error(e); 
